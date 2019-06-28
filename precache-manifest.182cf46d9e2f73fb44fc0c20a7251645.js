050dd6181c01dfe9fadedbcb4005d540428e4231
emre ayhan <emre.ayhan@adesso.com.tr>
2019-06-28 17:42:30 +0300

    Updates

---
+++ precache-manifest.182cf46d9e2f73fb44fc0c20a7251645.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a5c7a065cb059b03da71e51eee4d98",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "42a86b6a56e6374fbd5c",
+    "url": "/weather-api/static/css/main.6c6c7835.chunk.css"
+  },
+  {
+    "revision": "f04f2b9af6faa63ef672",
+    "url": "/weather-api/static/js/2.8cee3c0d.chunk.js"
+  },
+  {
+    "revision": "42a86b6a56e6374fbd5c",
+    "url": "/weather-api/static/js/main.6ad6a647.chunk.js"
+  },
+  {
+    "revision": "a104ea585fd9135dd611",
+    "url": "/weather-api/static/js/runtime~main.4827966c.js"
+  }
+]);
